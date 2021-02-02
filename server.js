7a3003c502f570e9a70b62e95ca1ce04d00607b2
Brendan Smith <brendan5320@gmail.com>
2021-02-02 12:31:16 -0800

    added save, deleteSaved, and getSavedResults functions to server.js

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const app = express();
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static('./public'));
 const DATABASE_URL = process.env.DATABASE_URL;
-const client = new pg.Client(process.env.DATABASE_URL);
+const client = new pg.Client(DATABASE_URL);
 
 // ===== other global variables ===== //
 const PORT = process.env.PORT || 3111;
@@ -19,7 +19,7 @@ const PORT = process.env.PORT || 3111;
 // ===== routes ===== //
 
 app.get('/', getHomeData);
-app.get('/search', getSearch);
+app.post('/', getSearch);
 app.get('/about', getAbout);
 app.get('/saved-results', getSavedResults);
 app.post('/search', save);
@@ -44,19 +44,44 @@ function getAbout(req, res) {
 
 function save(req, res) {
   //saving the the search results to the DB
-  //saved.ejs
+  //redirect to /saved-results?
+
+  // takes an 'ideam' object via POST on /search
+  const keyword = req.body
+  const sqlQuery = `INSERT INTO searches (keyword, google_results_count, niche_score) VALUES ($1, $2, $3) RETURNING id;`;
+  const sqlArray = [ideam.keyword, ideam.google_results_count, ideam.niche_score]
+  return client.query(sqlQuery, sqlArray).then(() => {
+    // notify user that ideam has been addedto database (using js to turn item blue?)
+    console.log(`added ${ideam.keyword} to database`);
+  })
 }
 
 function deleteSaved(req, res) {
   //delete saved 
   //saved.ejs
 
+  // takes DELETE on route /saved-results/:id 
+  const id = req.params.id;
+  // query db to delete item
+  const sqlQuery = `DELETE FROM searches WHERE id = $1;`;
+  const sqlArray = [id];
+  return client.query(sqlQuery, sqlArray).then(() => {
+    console.log(`deleted row ${id}`);
+    res.redirect('/saved-results');
+  })
 }
 
 function getSavedResults(req, res) {
   //take the user to the saved results page. 
   //saved.ejs
+  
+  // Query SQL db for all saved searches
+  const sqlQuery = `SELECT * FROM searches ORDER BY id;`;
+  return client.query(sqlQuery).then( result => {
+    res.render('pages/saved.ejs', {results : result.rows}); // Passes 'results' to saved.ejs
+  })
 }
+// ===== Helper Functions ===== // 
 
 // ===== other functions ===== //
 
