22d80c03005eee8c0fb9d6df7fbb63a5a77ee13a
JasonDormier <jmdormier@gmail.com>
2021-02-02 09:38:43 -0800

    route setup complete

--- server.js
+++ server.js
@@ -3,15 +3,15 @@
 const express = require('express');
 const superagent = require('superagent');
 require('dotenv').config();
-// const methodOverride = require('method-override');
-// const pg = require('pg');
+const methodOverride = require('method-override');
+const pg = require('pg');
 
 // ===== setup the app ===== //
 const app = express();
-app.use(express.urlencoded({ extended: true}));
+app.use(express.urlencoded({ extended: true }));
 app.use(express.static('./public'));
-// const DATABASE_URL = process.env.DATABASE_URL;
-// const client = new pg.Client(process.env.DATABASE_URL);
+const DATABASE_URL = process.env.DATABASE_URL;
+const client = new pg.Client(process.env.DATABASE_URL);
 
 // ===== other global variables ===== //
 const PORT = process.env.PORT || 3111;
@@ -19,11 +19,43 @@ const PORT = process.env.PORT || 3111;
 // ===== routes ===== //
 
 app.get('/', getHomeData);
+app.get('/search', getSearch);
+app.get('/about', getAbout);
+app.get('/saved-results', getSavedResults);
+app.post('/search', save);
+app.delete('/search', deleteSaved);
 
 // ===== helper functions ===== //
 
-function getHomeData(req, res){
+function getHomeData(req, res) {
+  //rendering all the search information
   res.render('./pages/index.ejs');
+}
+
+function getSearch(req, res) {
+  //getting the results from the search
+  //index.ejs
+}
+
+function getAbout(req, res) {
+  //sending the user to the about page
+  // about.ejs
+}
+
+function save(req, res) {
+  //saving the the search results to the DB
+  //saved.ejs
+}
+
+function deleteSaved(req, res) {
+  //delete saved 
+  //saved.ejs
+
+}
+
+function getSavedResults(req, res) {
+  //take the user to the saved results page. 
+  //saved.ejs
 }
 
 // ===== other functions ===== //
